d0c0d2136857dab7dc7658367d35965521823f57
bhawanabhatt-web <bbhawana131@gmail.com>
2026-07-13 19:13:42 +0545

    Task 4 client-server application has been completed.

--- Task4/protocol.h
+++ Task4/protocol.h
@@ -1,6 +1,5 @@
 #ifndef PROTOCOL_H
 #define PROTOCOL_H
-
 #define SERVER_PORT   8080
 #define BUF_SIZE      1024
 #define MAX_CLIENTS   10
@@ -8,6 +7,5 @@
 #define MAX_PASSWORD  64
 #define USERS_FILE    "users.txt"
 #define AUDIT_FILE    "server_audit.log"
-
 #endif
 
